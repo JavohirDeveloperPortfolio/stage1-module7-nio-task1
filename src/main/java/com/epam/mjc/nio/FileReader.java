5a4d5f734c00609f4a91d433213d481d3d6d928e
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-12-12 16:29:16 +0500

    done!

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))){
+            String s = reader.readLine();
+            profile.setName(s.substring(6));
+            s = reader.readLine();
+            profile.setAge(Integer.parseInt(s.substring(5,s.length())));
+            s = reader.readLine();
+            profile.setEmail(s.substring(7));
+            s = reader.readLine();
+            profile.setPhone(Long.parseLong(s.substring(7,s.length())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
